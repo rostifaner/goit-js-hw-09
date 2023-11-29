389164c55ba0be6654b511869c9b9484af5dcc97
Rostyslav Protsenko <rostifaner@gmail.com>
2023-11-29 03:45:31 +0200

    Add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const start = document.querySelector('[data-start]');
+const stop = document.querySelector('[data-stop]');
+const bodyColor = document.querySelector('body');
+const INTERVAL = 1000;
+stop.disabled = true;
+let timerColor;
 
+start.addEventListener('click', () => {
+  start.disabled = true;
+  stop.disabled = false;
+  changeColorInterval();
+});
+
+stop.addEventListener('click', () => {
+  clearInterval(timerColor);
+  start.disabled = false;
+  stop.disabled = true;
+});
+
+function changeColorInterval() {
+  timerColor = setInterval(() => {
+    bodyColor.style.backgroundColor = getRandomHexColor();
+  }, INTERVAL);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
